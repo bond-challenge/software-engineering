e2eb2ad777f24afeda1a7a886626b6ae2296562b
Rafael Gomes <contact@rafagomes.com>
2021-05-06 16:00:38 -0400

    Initial tests

---
+++ coding_question/tests/test_integer_list.py
@@ -0,0 +1,33 @@
+import unittest
+
+
+class TestIntegerList(unittest.TestCase):
+    def test_instantiate_class(self):
+        pass
+
+    def test_add_element(self):
+        pass
+
+    def test_typeerror_when_adding_float(self):
+        pass
+
+    def test_get_element(self):
+        pass
+
+    def test_get_remove_element(self):
+        pass
+
+    def test_insert_element(self):
+        pass
+
+    def test_typeerror_when_inserting_float(self):
+        pass
+
+    def test_remove_element(self):
+        pass
+
+    def test_clear_elements(self):
+        pass
+
+    def test_X_element(self):
+        pass
