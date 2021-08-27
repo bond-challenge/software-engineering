fc865b3acfb61d56db9064aca47e20847c01cdba
kushagra.dixit <kushagra.dixit@genaiz.com>
2021-08-27 10:21:16 -0400

    Added coding challenge comments and cleanup

--- kushagra-coding.py
+++ kushagra-coding.py
@@ -9,5 +9,5 @@ def moving_average(series, n):
 #testing array
 x = [87, 56, 16, 97, 45, 75, 41, 863, 90.2, 4]
 
-#testing the function
+#testing the function with window of 5, feel free to change the window size
 moving_average(x,5)
