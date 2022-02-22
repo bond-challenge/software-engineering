ccdc6f8bf8eb9edad01cd3f0f98928afcf59d679
George Oboh <georgeoboh@Georges-MacBook-Pro.local>
2022-02-22 11:33:56 -0500

    intial commit

---
+++ george_submission/Java Interface/README.md
@@ -0,0 +1,13 @@
+# Bond Challenge
+
+## Coding Question
+
+Coding Question - Answer
+
+## Interface 
+Interface for the data structure is created as com.bondbrand.challenge.test.FinalDataStructure. It provides the ability to store elements and provides the moving average of the last N elements added.
+
+## Implementation 
+An implementation provided is com.bondbrand.challenge.test.calcMovingAverage.
+
+It provides an implementation of FinalDataStructure for elements of type Float. The implementation class takes N, If N provided is less than or equal to zero, the moving average will not be provided.
---
+++ george_submission/Java Interface/calcMovingAverage.java
@@ -0,0 +1,91 @@
+package com.bondbrand.challenge.test;
+ 
+import java.util.LinkedList;
+import java.util.Queue;
+
+public interface FinalDataStructure {
+    void addElement(float element); /*Adds element in the data list*/
+    float getMovingAverage(); /*Returns moving average*/
+    public int getSize(); /*Returns size of the data list*/
+}
+
+/**
+ * Title: DoubleDataStore
+ * Description: Implementating FinalDataStructure for elements of any datatype.
+ 
+ * @author George Oboh
+*/
+public class calcMovingAverage implements FinalDataStructure {
+ 
+    private int n;
+    private Queue<Float> queue;
+    private float sum;
+    
+ 
+    public calcMovingAverage(int n) {
+        queue = new LinkedList<>();
+        this.n = n;
+    }
+ 
+    @Override
+    public void addElement(float element) {
+
+        /* Add the data element in the List */
+        queue.add(element);
+
+        if(n > 0)
+		{
+			/* Add the element to the sum */
+			sum += element;
+
+
+            /* If the number of elements in the Queue exceeds the N,
+            remove first element*/
+            if (queue.size() >= n) {
+                float dataToRemove = queue.poll();
+                sum -= dataToRemove;
+            }
+        } 
+        
+    }
+ 
+    @Override
+    public float getMovingAverage() {
+        //return sum / n;
+        /*If N is greater than zero and the list contains elements, return the average */
+		if(n > 0 && queue.size() > 0)
+		{
+			return (sum / n);
+		}
+		
+		/* Else */
+		return 0; /*null;*/
+    }
+	
+	/**
+	 * Returns the size of the data list.
+	 */
+	public int getSize()
+	{
+		return queue.size();
+	}
+//}
+
+//public class MainApp {
+    public static void main(String[] args) {
+        FinalDataStructure ma = new calcMovingAverage(4);
+ 
+        ma.addElement(3);
+        System.out.println(ma.getMovingAverage());
+        ma.addElement(7);
+        System.out.println(ma.getMovingAverage());
+        ma.addElement(4);
+        System.out.println(ma.getMovingAverage());
+        ma.addElement(2);
+        System.out.println(ma.getMovingAverage());
+        ma.addElement(1);
+        System.out.println(ma.getMovingAverage());
+        ma.addElement(2);
+        System.out.println(ma.getMovingAverage());
+    }
+}
